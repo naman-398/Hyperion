461d5280780e06627d645bb7a52fa41a117a6e54
naman-398 <you@example.com>
2024-05-24 10:33:20 +0530

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        grey_cloud: '#B6B6B4',
+        bee_yellow: 'DEB21B',
+        night: '#0B0B0B',
+        snow_drift: '#F9F9F7'
+      },
+      backgroundImage:{
+yellow_gradient: 'linear-gradient(97.23deg, #F2C922 -33.73%, #B8860B 100.34%)',
+text_gradient1: 'linear-gradient(145.14deg, #F9F9F7 32.81%, #656565 115.23%)',
+      },
+      fontFamily:{
+        poppins: '"Poppins", sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
