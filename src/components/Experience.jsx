1b7e7c705662a13a3145364df84b5c227a0e1476
Your Name <you@example.com>
2024-05-30 11:08:10 +0530

    update in back to top

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
             <div className='xl:mt-140 lg:mt-[100px] md:mt-[80px] mt-14 sm:mt-16 px-3'>
                 <div className='bg-experience rounded-[32px] bg-center bg-no-repeat max-w-[1488px] mx-auto  xl:py-[120px] lg:py-[95px] md:py-[70px] sm:py-[55px] py-[48px]'>
                     <div className='flex w-100 items-center justify-center lg:justify-start'>
-                        <div className=' lg:flex flex-col lg:pl-[84px] md:px-4 px-3'>
+                        <div className='lg:flex flex-col lg:pl-[84px] md:px-4 px-3'>
                             <h2 className='text-center lg:text-start  font-poppins text_gredient1 text-transparent bg-clip-text font-medium text-[26px] 576:text-[30px] sm:text-[38px] md:text-[44px] lg:text-[50px] xl:text-[56px] md:leading-[56px] leading-[36px] 576:leading-[40px] sm:leading-[46px]  max-w-[692px]'>Experience the hyperion difference</h2>
                             <p className='text-center max-lg:mx-auto lg:text-start mt-3 sm:mt-5 md:mt-6 xl:mt-8 font-poppins text-snow_drift max-w-[618px] opacity-70 text-[16px] lg:text-[18px] leading-[23.4px] font-normal'>Ready to unlock a whole new dimension and take your trading to new levels? Experience the Hyperion difference today.
                             </p>
