0f5cb37be5fc2961b6c261880cfd23c85eef1824
Your Name <you@example.com>
2024-05-31 11:35:06 +0530

    update in feature section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -15,7 +15,7 @@ const Features = () => {
                                 <div className={`${index == 0 ? " max-[1200px]:flex items-center justify-center min-[1200px]:w-[32.3%] w-full md:w-[50%]  md:pr-3" : index == 1 ? "md:border-l-[1px] border-l-[#1E1E1F] min-[1200px]:border-r-[1px] mt-12 md:mt-0 flex md:pl-3 min-[1200px]:px-3 items-center justify-center min-[1200px]:w-[35.4%] w-full md:w-[50%] border-r-[#1E1E1F]" : index == 2 ? " min-[1200px]:w-[32.3%] w-full min-[1200px]:pl-3 flex items-center justify-center max-[1200px]:mt-12 min-[1200px]:items-end flex-col" : ""} h-full`}>
                                     <div className=" min-[1200px]:max-w-[392px] max-w-[450px]">
                                         <img src={obj.img} alt="img" className="w-[392px] h-[294px]" />
-                                        <p className='text-center md:text-start  text_gredient1 text-transparent bg-clip-text font-medium text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] lg:mt-12 mt-5 md:mt-9 lg:mb-6 mb-3 font-poppins'>{obj.title}</p>
+                                        <p className='text-center md:text-start  text_gredient1 text-transparent bg-clip-text font-medium text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] lg:mt-12 mt-5 md:mt-9 lg:pb-6 pb-3 font-poppins'>{obj.title}</p>
                                         <p className='text-center md:text-start font-poppins text-snow_drift opacity-70 text-[14px] md:text-[16px] leading-[20.8px] font-normal max-w-[392px]'>{obj.description}</p>
                                     </div>
                                 </div>
@@ -25,7 +25,7 @@ const Features = () => {
                     <div className=" flex flex-col-reverse min-[1380px]:flex-row flex-wrap pt-12">
                         <div className="max-[1380px]:mt-12 max-[1380px]:flex items-center justify-center min-[1380px]:w-[32.3%] h-full ">
                             <div className=" max-w-[392px] aiDrivencard">
-                                <p className='text-center min-[1380px]:text-start  text_gredient1 text-transparent bg-clip-text font-medium text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] lg:mb-6 mb-3 font-poppins'>Integrations</p>
+                                <p className='text-center min-[1380px]:text-start  text_gredient1 text-transparent bg-clip-text font-medium text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] lg:pb-6 pb-3 font-poppins'>Integrations</p>
                                 <p className='text-center min-[1380px]:text-start font-poppins text-snow_drift opacity-70 text-[14px] md:text-[16px] leading-[20.8px] font-normal max-w-[392px]'>GMX, SYNTHENTIX, AGGR TRADE, 0x Protocol, TradingView </p>
                                 <div className=" max-w-[392px] xl:mt-12 lg:mt-10 md:mt-8 mt-6 max-h-[294px] overflow-hidden rounded-3xl border-[1px] border-solid  border-[#F9F9F714]"><img src={graphimg} alt="img" className=" w-full h-full aiDrivenimg duration-300 " /></div>
                             </div>
@@ -34,7 +34,7 @@ const Features = () => {
                             <div className=" flex flex-col lg:flex-row md:gap-10 gap-6 items-center justify-center lg:gap-12">
                                 <div className=" max-w-[302px] w-full sm:max-w-[392px] max-h-[300px] sm:max-h-[400px] md:max-h-[470px] lg:min-h-[520px] h-full overflow-hidden rounded-3xl border-[1px] border-solid  border-[#F9F9F714]"><img src={graphimg2} alt="img" className=" lg:min-h-[520px] w-full h-full aiDrivenimg duration-300 " /></div>
                                 <div className=" flex flex-col max-w-[600px]">
-                                    <p className='text-center lg:text-start  text_gredient1 text-transparent bg-clip-text font-medium text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] lg:mb-6 md:mb-4 mb-3 font-poppins'>AI Trading Indicators
+                                    <p className='text-center lg:text-start  text_gredient1 text-transparent bg-clip-text font-medium text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] lg:pb-6 md:pb-4 pb-3 font-poppins'>AI Trading Indicators
                                     </p>
                                     <p className='text-center lg:text-start font-poppins text-snow_drift opacity-70 text-[14px] md:text-[16px] leading-[20.8px] font-normal'>Gain a competitive edge with Hyperion's AI trading indicator. Identify the most promising trading pairs and leverage AI-driven analysis for strategic, low-risk trades. </p>
                                     <p className='text-center lg:text-start mt-3 font-poppins text-snow_drift opacity-70 text-[14px] md:text-[16px] leading-[20.8px] font-normal'>Here’s a breakdown of 10+ trading indicators you’ll get from Hyperion AI for each of your trades: </p>
